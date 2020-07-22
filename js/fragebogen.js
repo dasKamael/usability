f5def651b8e02302260475c4f7b6f29733710d93
dasKameltreiber <richard.maasri@hotmail.de>
2020-07-22 17:57:53 +0200

    Zusammenfassung gefixed

--- js/fragebogen.js
+++ js/fragebogen.js
@@ -1,5 +1,6 @@
 var slider = document.getElementsByClassName('slider');
 var sliderResult = document.getElementsByClassName('sliderResult');
+document.getElementById('loggedUser').innerHTML = localStorage.getItem('user');
 var current = localStorage.getItem('current').split(',');
 
 var prof = document.getElementById("profName").innerHTML = current[1];
@@ -7,6 +8,13 @@ var lecture = document.getElementById("lecture").innerHTML = current[0];
 var currentQuestionPage = document.getElementById('currentQuestionPage').innerText;
 
 var questionsAnswered = [];
+
+var color1 = "color: #cb3333";
+var color2 = "color: #B24D26";
+var color3 = "color: darkorange";
+var color4 = "color: #7F800D";
+var color5 = "color: #669900";
+
 
 
 if(localStorage.questionAnswers == null){
@@ -21,24 +29,24 @@ Array.from(sliderResult).forEach(function(e){
     switch(questionsAnswered[Array.from(sliderResult).indexOf(e) + (parseInt(currentQuestionPage)-1)*5]){
         case "1": 
             e.innerText = "Trifft überhaupt nicht zu";
-            e.style.cssText = "color: #cb3333";
+            e.style.cssText = color1;
             console.log(Array.from(slider).indexOf(e));
             break;
         case "2":
             e.innerText = "Trifft nicht zu";
-            e.style.cssText = "color: #B24D26";
+            e.style.cssText = color2;
             break;
         case "3":
             e.innerText = "Trifft eher nicht zu";
-            e.style.cssText = "color: darkorange";
+            e.style.cssText = color3;
             break;
         case "4":
             e.innerText = "Trifft zu";
-            e.style.cssText = "color: #7F800D";
+            e.style.cssText = color4;
             break;
         case "5":
             e.innerText = "Trifft sehr zu";
-            e.style.cssText = "color: #669900";
+            e.style.cssText = color5;
             break;
         default: console.log("gibts nicht");
     }
@@ -49,24 +57,24 @@ Array.from(slider).forEach(function(e) {
     switch(questionsAnswered[Array.from(slider).indexOf(e) + (parseInt(currentQuestionPage)-1)*5]){
         case "1": 
             e.value = 1;
-            e.style.cssText = "color: #cb3333";
+            e.style.cssText = color1;
             console.log(Array.from(slider).indexOf(e));
             break;
         case "2":
             e.value = 2;
-            e.style.cssText = "color: #B24D26";
+            e.style.cssText = color2;
             break;
         case "3":
             e.value = 3;
-            e.style.cssText = "color: darkorange";
+            e.style.cssText = color3;
             break;
         case "4":
             e.value = 4;
-            e.style.cssText = "color: #7F800D";
+            e.style.cssText = color4;
             break;
         case "5":
             e.value = 5;
-            e.style.cssText = "color: #669900";
+            e.style.cssText = color5;
             break;
         default: console.log("gibts nicht");
     }
@@ -79,27 +87,27 @@ Array.from(slider).forEach(function(e) {
         switch(value){
             case "1": 
                 resultNode.innerHTML = "Trifft überhaupt nicht zu";
-                resultNode.style.cssText = "color: #cb3333";
+                resultNode.style.cssText = color1;
                 questionsAnswered[pos] = '1';
                 break;
             case "2":
                 resultNode.innerHTML = "Trifft nicht zu";
-                resultNode.style.cssText = "color: #B24D26";
+                resultNode.style.cssText = color2;
                 questionsAnswered[pos] = '2';
                 break;
             case "3":
                 resultNode.innerHTML = "Trifft eher nicht zu";
-                resultNode.style.cssText = "color: darkorange";
+                resultNode.style.cssText = color3;
                 questionsAnswered[pos] = '3';
                 break;
             case "4":
                 resultNode.innerHTML = "Trifft zu";
-                resultNode.style.cssText = "color: #7F800D";
+                resultNode.style.cssText = color4;
                 questionsAnswered[pos] = '4';
                 break;
             case "5":
                 this.nextElementSibling.innerHTML = "Trifft sehr zu";
-                resultNode.style.cssText = "color: #669900";
+                resultNode.style.cssText = color5;
                 questionsAnswered[pos] = '5';
                 break;
             default: console.log("gibts nicht");
--- js/zusammenfassung.js
+++ js/zusammenfassung.js
@@ -1,31 +1,41 @@
 var slider = document.getElementsByClassName('slider');
 var lecture = document.getElementById("lecture").innerHTML = localStorage.getItem('current');
 var send = document.getElementById('nextPageButton');
+var current = localStorage.getItem('current').split(',');
+
+var prof = document.getElementById("profName").innerHTML = current[1];
+var lecture = document.getElementById("lecture").innerHTML = current[0];
 var questionsAnswered = localStorage.getItem('questionAnswers').split(',');
+
+var color1 = "color: #cb3333";
+var color2 = "color: #B24D26";
+var color3 = "color: darkorange";
+var color4 = "color: #7F800D";
+var color5 = "color: #669900";
 
 Array.from(slider).forEach(function(e) {
     let resultNode = e.parentNode.parentNode.firstElementChild
     switch(questionsAnswered[Array.from(slider).indexOf(e)]){
         case "1": 
             resultNode.innerText = "Trifft überhaupt nicht zu";
-            resultNode.style.cssText = "color: #cb3333";
+            resultNode.style.cssText = color1;
             console.log(Array.from(slider).indexOf(e));
             break;
         case "2":
             resultNode.innerText = "Trifft nicht zu";
-            resultNode.style.cssText = "color: #B24D26";
+            resultNode.style.cssText = color2;
             break;
         case "3":
             resultNode.innerText = "Trifft eher nicht zu";
-            resultNode.style.cssText = "color: darkorange";
+            resultNode.style.cssText = color3;
             break;
         case "4":
             resultNode.innerText = "Trifft zu";
-            resultNode.style.cssText = "color: #7F800D";
+            resultNode.style.cssText = color4;
             break;
         case "5":
             resultNode.innerText = "Trifft sehr zu";
-            resultNode.style.cssText = "color: #669900";
+            resultNode.style.cssText = color5;
             break;
         default: console.log("gibts nicht");
     }
@@ -36,27 +46,27 @@ Array.from(slider).forEach(function(e) {
         switch(value){
             case "1": 
                 resultNode.innerText = "Trifft überhaupt nicht zu";
-                resultNode.style.cssText = "color: #cb3333";
+                resultNode.style.cssText = color1;
                 questionsAnswered[Array.from(slider).indexOf(e)] = '1';
                 break;
             case "2":
                 resultNode.innerText = "Trifft nicht zu";
-                resultNode.style.cssText = "color: #B24D26";
+                resultNode.style.cssText = color2;
                 questionsAnswered[Array.from(slider).indexOf(e)] = '2';
                 break;
             case "3":
                 resultNode.innerText = "Trifft eher nicht zu";
-                resultNode.style.cssText = "color: darkorange";
+                resultNode.style.cssText = color3;
                 questionsAnswered[Array.from(slider).indexOf(e)] = '4';
                 break;
             case "4":
                 resultNode.innerText = "Trifft zu";
-                resultNode.style.cssText = "color: #7F800D";
+                resultNode.style.cssText = color4;
                 questionsAnswered[Array.from(slider).indexOf(e)] = '4';
                 break;
             case "5":
                 resultNode.innerText = "Trifft sehr zu";
-                resultNode.style.cssText = "color: #669900";
+                resultNode.style.cssText = color5;
                 questionsAnswered[Array.from(slider).indexOf(e)] = '5';
                 break;
             default: console.log("gibts nicht");
